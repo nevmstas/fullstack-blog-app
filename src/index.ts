4bef9c75e7b547329a4f4f1691dc5880faedecd4
stasnevm <nevmyvakastas@gmail.com>
2021-01-10 23:20:11 +0600

    configure mikro-orm, migrations, scripts

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroOrmConfig from './mikro-orm.config'
+
+const main = async () => {
+  const orm = await MikroORM.init(mikroOrmConfig);
+  await orm.getMigrator().up()
+
+  //adding post
+  // const post = orm.em.create(Post, {title: 'first post'})
+  // await orm.em.persistAndFlush(post)
+
+  const posts = await orm.em.find(Post, {})
+  console.log(posts)
+};
+
+main().catch((err)=>{
+    console.error(err)
+})
---
+++ src/migrations/Migration20210110170742.ts
@@ -0,0 +1,9 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20210110170742 extends Migration {
+
+  async up(): Promise<void> {
+
+  }
+
+}
