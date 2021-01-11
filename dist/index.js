80963461bd3c90b62ac00e6d223dba635a2663da
stasnevm <nevmyvakastas@gmail.com>
2021-01-11 23:02:17 +0600

    graphql setup, ts

--- dist/index.js
+++ dist/index.js
@@ -13,13 +13,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const type_graphql_1 = require("type-graphql");
+const hello_1 = require("./resolvers/hello");
+const PORT = 4000;
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const posts = yield orm.em.find(Post_1.Post, {});
-    console.log(posts);
+    const app = express_1.default();
+    const appoloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [hello_1.HelloResolver],
+            validate: false
+        }),
+    });
+    appoloServer.applyMiddleware({ app });
+    app.listen(PORT, () => {
+        console.log(`server started on localhost:${PORT}`);
+    });
 });
 main().catch((err) => {
     console.error(err);
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,34 @@
 import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
-import mikroOrmConfig from './mikro-orm.config'
+// import { Post } from "./entities/Post";
+import mikroOrmConfig from "./mikro-orm.config";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
+import { HelloResolver } from "./resolvers/hello";
+
+const PORT = 4000;
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
-  await orm.getMigrator().up()
+  await orm.getMigrator().up();
 
-  //adding post
-  // const post = orm.em.create(Post, {title: 'first post'})
-  // await orm.em.persistAndFlush(post)
+  const app = express();
 
-  const posts = await orm.em.find(Post, {})
-  console.log(posts)
+  const appoloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false
+    }),
+  });
+
+  appoloServer.applyMiddleware({ app });
+
+  app.listen(PORT, () => {
+    console.log(`server started on localhost:${PORT}`);
+  });
 };
 
-main().catch((err)=>{
-    console.error(err)
-})
+main().catch((err) => {
+  console.error(err);
+});
